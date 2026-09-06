fb5a2b54d576addd949ac918e787a1cc3d0b8edd
Mario Kicherer <dev@kicherer.org>
2026-09-06 20:14:27 +0200

    Harden tests and source distribution

--- tests/test.sh
+++ tests/test.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
+set -euo pipefail
+
+TEST_DIR=$(CDPATH= cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
+
 qgrep() {
-	grep $1 $2 >/dev/null || return 1
+	grep -q -- "$1" "$2"
 }
 
 cleanup() {
-	fusermount -zu ${FDIR}
-	rmdir ${FDIR}
+	fusermount -zu "$FDIR" >/dev/null 2>&1 || true
+	rmdir "$FDIR" >/dev/null 2>&1 || true
 }
 
 fail() {
@@ -14,69 +18,66 @@ fail() {
 	lineno=$2
 	shift
 	shift
-	echo "In $src:$lineno: \"$(cat $src | head -n $lineno | tail -n1)\""
-	tree ${FDIR}
+	echo "In $src:$lineno: \"$(sed -n "${lineno}p" "$src")\""
+	tree "$FDIR"
 	echo "ERROR"
 	exit 1
 }
 
-FDIR=fuse
-
-mkdir ${FDIR}
-../sparsefs -s $(pwd)/test1/src1/ -s $(pwd)/test1/src2/ \
-	${FDIR}
-
+FDIR=$(mktemp -d "${TMPDIR:-/tmp}/sparsefs-test.XXXXXX")
 trap cleanup EXIT
 
-qgrep source1 ${FDIR}/both12 || fail ${BASH_SOURCE} ${LINENO}
-qgrep source1 ${FDIR}/path12/both12 || fail ${BASH_SOURCE} ${LINENO}
+"$TEST_DIR/../sparsefs" -s "$TEST_DIR/test1/src1/" -s "$TEST_DIR/test1/src2/" \
+	"$FDIR"
 
-[ "$(ls -1 ${FDIR} | grep both12 | wc -l)" != "1" ] && fail ${BASH_SOURCE} ${LINENO}
+qgrep source1 "$FDIR/both12" || fail "${BASH_SOURCE[0]}" "$LINENO"
+qgrep source1 "$FDIR/path12/both12" || fail "${BASH_SOURCE[0]}" "$LINENO"
 
-ls ${FDIR} | qgrep source1 || fail ${BASH_SOURCE} ${LINENO}
-ls ${FDIR} | qgrep source2 || fail ${BASH_SOURCE} ${LINENO}
+[ "$(find "$FDIR" -maxdepth 1 -name both12 | wc -l)" != "1" ] && fail "${BASH_SOURCE[0]}" "$LINENO"
 
-qgrep source1 ${FDIR}/source1 && qgrep source2 ${FDIR}/source2 || fail ${BASH_SOURCE} ${LINENO}
+find "$FDIR" -maxdepth 1 -printf '%f\n' | qgrep source1 || fail "${BASH_SOURCE[0]}" "$LINENO"
+find "$FDIR" -maxdepth 1 -printf '%f\n' | qgrep source2 || fail "${BASH_SOURCE[0]}" "$LINENO"
 
-ls ${FDIR}/path12 | qgrep source1 || fail ${BASH_SOURCE} ${LINENO}
-ls ${FDIR}/path12 | qgrep source2 || fail ${BASH_SOURCE} ${LINENO}
-[ "$(ls -1 ${FDIR}/path12 | wc -l)" != "3" ] && fail ${BASH_SOURCE} ${LINENO}
+qgrep source1 "$FDIR/source1" && qgrep source2 "$FDIR/source2" || fail "${BASH_SOURCE[0]}" "$LINENO"
 
-[ "$(ls -1 ${FDIR}/path1 | wc -l)" != "1" ] && fail ${BASH_SOURCE} ${LINENO}
+find "$FDIR/path12" -maxdepth 1 -printf '%f\n' | qgrep source1 || fail "${BASH_SOURCE[0]}" "$LINENO"
+find "$FDIR/path12" -maxdepth 1 -printf '%f\n' | qgrep source2 || fail "${BASH_SOURCE[0]}" "$LINENO"
+[ "$(find "$FDIR/path12" -mindepth 1 -maxdepth 1 | wc -l)" != "3" ] && fail "${BASH_SOURCE[0]}" "$LINENO"
 
-qgrep source1 ${FDIR}/path12/source1 && qgrep source2 ${FDIR}/path12/source2 || fail ${BASH_SOURCE} ${LINENO}
+[ "$(find "$FDIR/path1" -mindepth 1 -maxdepth 1 | wc -l)" != "1" ] && fail "${BASH_SOURCE[0]}" "$LINENO"
 
-qgrep source1 ${FDIR}/path1/source1 || fail ${BASH_SOURCE} ${LINENO}
-qgrep source2 ${FDIR}/path2/source2 || fail ${BASH_SOURCE} ${LINENO}
+qgrep source1 "$FDIR/path12/source1" && qgrep source2 "$FDIR/path12/source2" || fail "${BASH_SOURCE[0]}" "$LINENO"
+
+qgrep source1 "$FDIR/path1/source1" || fail "${BASH_SOURCE[0]}" "$LINENO"
+qgrep source2 "$FDIR/path2/source2" || fail "${BASH_SOURCE[0]}" "$LINENO"
 
 cleanup
 
+FDIR=$(mktemp -d "${TMPDIR:-/tmp}/sparsefs-test.XXXXXX")
+"$TEST_DIR/../sparsefs" -s "$TEST_DIR/test1/src1/" -s "$TEST_DIR/test1/src2/" \
+	-X "$TEST_DIR/test1/src1/both12" \
+	-X "$TEST_DIR/test1/src1/path12/both12" \
+	"$FDIR"
 
-mkdir ${FDIR}
-../sparsefs -s $(pwd)/test1/src1/ -s $(pwd)/test1/src2/ \
-	-X $(pwd)/test1/src1/both12 \
-	-X $(pwd)/test1/src1/path12/both12 \
-	${FDIR}
+qgrep source2 "$FDIR/both12" || fail "${BASH_SOURCE[0]}" "$LINENO"
+qgrep source2 "$FDIR/path12/both12" || fail "${BASH_SOURCE[0]}" "$LINENO"
 
-qgrep source2 ${FDIR}/both12 || fail ${BASH_SOURCE} ${LINENO}
-qgrep source2 ${FDIR}/path12/both12 || fail ${BASH_SOURCE} ${LINENO}
+[ "$(find "$FDIR" -maxdepth 1 -name both12 | wc -l)" != "1" ] && fail "${BASH_SOURCE[0]}" "$LINENO"
 
-[ "$(ls -1 ${FDIR} | grep both12 | wc -l)" != "1" ] && fail ${BASH_SOURCE} ${LINENO}
+find "$FDIR" -maxdepth 1 -printf '%f\n' | qgrep source1 || fail "${BASH_SOURCE[0]}" "$LINENO"
+find "$FDIR" -maxdepth 1 -printf '%f\n' | qgrep source2 || fail "${BASH_SOURCE[0]}" "$LINENO"
 
-ls ${FDIR} | qgrep source1 || fail ${BASH_SOURCE} ${LINENO}
-ls ${FDIR} | qgrep source2 || fail ${BASH_SOURCE} ${LINENO}
+qgrep source1 "$FDIR/source1" && qgrep source2 "$FDIR/source2" || fail "${BASH_SOURCE[0]}" "$LINENO"
 
-qgrep source1 ${FDIR}/source1 && qgrep source2 ${FDIR}/source2 || fail ${BASH_SOURCE} ${LINENO}
+find "$FDIR/path12" -maxdepth 1 -printf '%f\n' | qgrep source1 || fail "${BASH_SOURCE[0]}" "$LINENO"
+find "$FDIR/path12" -maxdepth 1 -printf '%f\n' | qgrep source2 || fail "${BASH_SOURCE[0]}" "$LINENO"
+[ "$(find "$FDIR/path12" -mindepth 1 -maxdepth 1 | wc -l)" != "3" ] && fail "${BASH_SOURCE[0]}" "$LINENO"
 
-ls ${FDIR}/path12 | qgrep source1 || fail ${BASH_SOURCE} ${LINENO}
-ls ${FDIR}/path12 | qgrep source2 || fail ${BASH_SOURCE} ${LINENO}
-[ "$(ls -1 ${FDIR}/path12 | wc -l)" != "3" ] && fail ${BASH_SOURCE} ${LINENO}
+[ "$(find "$FDIR/path1" -mindepth 1 -maxdepth 1 | wc -l)" != "1" ] && fail "${BASH_SOURCE[0]}" "$LINENO"
 
-[ "$(ls -1 ${FDIR}/path1 | wc -l)" != "1" ] && fail ${BASH_SOURCE} ${LINENO}
+qgrep source1 "$FDIR/path12/source1" && qgrep source2 "$FDIR/path12/source2" || fail "${BASH_SOURCE[0]}" "$LINENO"
 
-qgrep source1 ${FDIR}/path12/source1 && qgrep source2 ${FDIR}/path12/source2 || fail ${BASH_SOURCE} ${LINENO}
+qgrep source1 "$FDIR/path1/source1" || fail "${BASH_SOURCE[0]}" "$LINENO"
+qgrep source2 "$FDIR/path2/source2" || fail "${BASH_SOURCE[0]}" "$LINENO"
 
-qgrep source1 ${FDIR}/path1/source1 || fail ${BASH_SOURCE} ${LINENO}
-qgrep source2 ${FDIR}/path2/source2 || fail ${BASH_SOURCE} ${LINENO}
-
-tree ${FDIR}
+find "$FDIR" -print
